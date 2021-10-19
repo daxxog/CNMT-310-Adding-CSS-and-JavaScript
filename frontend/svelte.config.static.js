17cb095cefe9b78e402b010c4f0e9eff6ebf3347
David Volm <david@volminator.com>
2021-10-18 20:00:04 -0500

    Adjust path for static build

--- frontend/svelte.config.static.js
+++ frontend/svelte.config.static.js
@@ -3,6 +3,9 @@ import adapter from '@sveltejs/adapter-static';
 
 export default {
     kit: {
+        paths: {
+            base: '/~dvolm359/cnmt-310-adding-css-and-javascript'
+        },
         adapter: adapter({
             // default options are shown
             pages: 'build',
